8995e916555054e3e812a10ee7173ff5bc8bc2be
lastchance <james.jy.you@gmail.com>
2019-04-18 19:16:48 -0700

    Get Tool

---
+++ PythonScraping/get_mtgatool_data.py
@@ -0,0 +1,68 @@
+import requests
+import hashlib
+import time
+
+S = requests.Session()
+
+url ='https://mtgatool.com/api/'
+rslt = S.post(url+"login.php", data={'email':'YOURUSERNAME', 'password':
+                                     hashlib.sha1('YOURPASSWORD'.encode()).hexdigest(),
+                                     'playername':'', 'playerid':'',
+                                     'mtgaversion':'', 'playerid':'',
+                                     'version':'', 'reqId':'ABCDEF',
+                                     'method':'auth',
+                                     'method_path':'/api/login.php'})
+
+token = rslt.json()['token']
+
+data = []
+data_ids = set()
+
+decks = {}
+
+# 100 is the number of sets of 25 decklists to retrieve
+for ii in range(100):
+    time.sleep(1) # give the server a break, sleep between queries
+
+    skip = ii * 25
+
+    # do not use any filters - it's apparently a lighter load for the server that way
+    result = S.post(url+"get_explore.php",
+                    data={'token': token, 'filter_wcc': "", 'filter_wcu': "",
+                          'filter_sortdir': 1, 'filter_type': '',
+                          'filter_sort':"By Date", 'filter_skip':str(skip),
+                          'filter_owned':"false", 'filter_event':"",
+                          "filter_wcr":"", "filter_wcm":"", })
+
+    data_this = result.json()
+
+    unique_ids = set([x['_id'] for x in data_this['result']])
+
+    n_unique = len(unique_ids - data_ids)
+
+    data += data_this['result']
+
+    data_ids = data_ids | unique_ids
+
+    print(f"Added {n_unique} new ids of {len(unique_ids)} retrieved, total {len(data)}")
+
+    # download each deck / match result entry
+    for entry in data_this['result']:
+        time.sleep(1) # again, give the server a break
+        deckid = entry['_id']
+
+        course = S.post(url+"get_course.php", data={'token': token, 'courseid':deckid})
+        course.raise_for_status()
+        assert course.json()['ok']
+
+        decks[deckid] = course.json()
+
+        print(".", end="", flush=True)
+
+
+# then do stuff with the data (I'm not sure what yet, but it might be possible
+# to get actual matchup data by cross-matching IDs)
+all_match_ids = [x
+                 for deckid in decks
+                 for x in decks[deckid]['result']['ModuleInstanceData']['WinLossGate']['ProcessedMatchIds']
+                ]
